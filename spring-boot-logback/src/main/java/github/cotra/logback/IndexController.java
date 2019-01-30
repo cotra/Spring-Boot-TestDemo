bdf7977a304a4e3a9b9df32475664a37b0c44a44
linyaqi <mr03web@foxmail.com>
2019-01-30 14:15:25 +0800

    spring-boot-logback init

---
+++ spring-boot-logback/src/main/java/github/cotra/logback/IndexController.java
@@ -0,0 +1,13 @@
+package github.cotra.logback;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "spring-boot-logback";
+    }
+}
